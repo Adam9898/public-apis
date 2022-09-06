6f757b204887044bfd83132922e775ccf8841af7
Adam9898 <adam.reichardt@protonmail.com>
2022-09-06 18:56:12 +0200

    homepage text edit

--- src/app/agify-name-age/agify-name-age.component.ts
+++ src/app/agify-name-age/agify-name-age.component.ts
@@ -12,6 +12,8 @@ export class AgifyNameAgeComponent implements OnInit {
   @ViewChild('f') nameForm: NgForm;
   public name: string;
   public age: number;
+  public gender: 'male' | 'female';
+  public nationality: string;
 
   constructor(private httpClient: HttpClient) {
 
@@ -22,10 +24,39 @@ export class AgifyNameAgeComponent implements OnInit {
 
   onSubmit() {
     this.age = null;
+    this.gender = null;
+    this.nationality = null;
     this.name = this.nameForm.value.name;
     this.httpClient.get('https://api.agify.io/?name=' + this.name)
       .subscribe((agifyApiObj: {name: string, age: number}) => this.age = agifyApiObj.age);
-    this.nameForm.resetForm();
+    if (this.nameForm.value.gender) {
+      this.checkGender();
+    }
+    if (this.nameForm.value.nationality) {
+      this.checkNationality()
+    }
+
   }
 
+  checkGender() {
+    this.httpClient.get('https://api.genderize.io/?name=' + this.name)
+      .subscribe((res: {gender: 'male' | 'female'}) => {
+        this.gender = res.gender;
+      })
+  }
+
+  checkNationality() {
+    this.httpClient.get('https://api.nationalize.io/?name=' + this.name)
+      .subscribe((res: {country: {country_id: string}[]}) => {
+        const nation = new Intl.DisplayNames(['en'], {type: 'region'});
+        // country[0] contains the highest probability nation
+        this.nationality = nation.of(res.country[0].country_id);
+      });
+  }
+
+  onClearResults() {
+    this.age = null;
+    this.gender = null;
+    this.nationality = null;
+  }
 }
